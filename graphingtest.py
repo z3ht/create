26af70886695e177511742c7f267dd07e9d01193
Andrew Darling <andr3w.darling@gmail.com>
2018-12-05 14:27:28 -0700

    Change grapher and graphing test names

--- graphingtest.py
+++ graphingtest.py
@@ -1,8 +1,8 @@
-import Grapher
+import grapher
 
-graph = Grapher.Graph(Grapher.Settings())
+graph = grapher.Graph(grapher.Settings())
 
-plot = Grapher.Plot(graph)
+plot = grapher.Plot(graph)
 plot.plotTxt("sampledata.txt")
 
 graph.registerPlot(plot)
@@ -10,12 +10,12 @@ graph.registerPlot(plot)
 # Turtle does not support multiple windows opening at once,
 # so this should graph on top of the previous graph with a
 # new scale
-graph2 = Grapher.Graph(Grapher.Settings())
+graph2 = grapher.Graph(grapher.Settings())
 
-plot2 = Grapher.Plot(graph2)
+plot2 = grapher.Plot(graph2)
 plot2.plotEquation(lambda x: x**2)
 
-plot3 = Grapher.Plot(graph2)
+plot3 = grapher.Plot(graph2)
 plot3.plotEquation(lambda x: x**3)
 
 graph2.registerPlot(plot2)
@@ -24,7 +24,7 @@ graph2.registerPlot(plot3)
 graph.drawPlots()
 graph2.drawPlots()
 
-data = Grapher.Data(plot)
+data = grapher.Data(plot)
 print("Max: %d" % data.getMax())
 print("Avg: %d" % data.getAvg())
 
